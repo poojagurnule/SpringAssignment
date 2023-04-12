172a690c6a05c0584c27098902590e902f2d87a7
Pooja Gurnule <poojagurnule2411999@gmail.com>
2023-04-12 08:28:27 +0530

    Assignment Done

---
+++ FetchDataAssignment/FetchDataAssign2.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class FetchDataAssign2 {
+    public static void main(String[] args) throws Exception {
+
+
+        String apiUrl = " https://api.zippopotam.us/us/33162";
+        URL url = new URL(apiUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+
+        int responseCode = conn.getResponseCode();
+        if (responseCode == 200) {
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            System.out.println(response.toString() );
+        }else{
+            System.out.println("This is not valid URL- " + responseCode);
+        }
+
+    }
+}
